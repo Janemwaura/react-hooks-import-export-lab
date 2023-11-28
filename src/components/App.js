203ba5074030fcc3469f44d5293162634e3f90ef
Janemwaura <mwaura461@gmail.com>
2023-11-28 21:36:20 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import NavBar from "../components/NavBar.js";
+import Home from "../components/Home.js";
+import About from "../components/About.js";
 function App() {
   return (
     <div>
@@ -11,3 +13,4 @@ function App() {
 }
 
 export default App;
+
